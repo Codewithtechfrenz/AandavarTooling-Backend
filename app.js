68df9ed4dd65e2a443514da1448fba72fcd619fe
PrabhuNaga <nagaprabhu.balu95@gmail.com>
2026-03-25 12:38:46 +0530

    add gitignore

--- app.js
+++ app.js
@@ -141,7 +141,7 @@ app.use('/workorder', workOrderRouter);
 
 // ================== HEALTH CHECK ==================
 app.get('/', (req, res) => {
-  res.json({ status: 1, message: "API running successfully", version: "1.0.2" });
+  res.json({ status: 1, message: "API running successfully", version: "1.0.3" });
 });
 
 // ================== 404 HANDLER ==================
