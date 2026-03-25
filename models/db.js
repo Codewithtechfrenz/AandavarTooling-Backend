d2b29b97d9b76853b20cf2d782a30bfd5e945302
Codewithtechfrenz <techfrenz2025@gmail.com>
2026-03-25 15:44:26 +0530

    Updated db

--- models/db.js
+++ models/db.js
@@ -17,7 +17,7 @@ const mysql = require("mysql");
 const pool = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "",
+    password: "Aandavar@2026!#$",
     database: "aandavartool",
    // port: 3306,
     connectionLimit: 50,
